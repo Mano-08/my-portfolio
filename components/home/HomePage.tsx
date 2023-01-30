1cb38f631dc35966b4e8bc470fd53e083e26d999
Mano <mano.26q@gmail.com>
2023-01-31 00:07:37 +0530

    added flex

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 import classes from "./HomePage.module.scss";
 
 function HomePage() {
-  return <div></div>;
+  return (
+    <div id="landingPage" className={classes.landingPageContainer}>
+      <section className={classes.aboutMeContainer}>
+        <div className={classes.myNameContainer}>Hi Iam Mano</div>
+        <div className={classes.balloonsContainer}>Balloons Section</div>
+      </section>
+      <section className={classes.grass}></section>
+      <section className={classes.ground}></section>
+    </div>
+  );
 }
 
 export default HomePage;
