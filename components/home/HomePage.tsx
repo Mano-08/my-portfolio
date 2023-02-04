43c61ad8424a7197b71b52e250ef554fdf67ec56
Mano <mano.26q@gmail.com>
2023-02-05 02:57:23 +0530

    added myName section

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -3,25 +3,39 @@ import Image from "next/image";
 import styles from "./HomePage.module.scss";
 import balloonYellow from "./images/yellowBalloon.svg";
 import balloonOrange from "./images/orangeBalloon.svg";
-import { Typewriter } from "react-simple-typewriter";
 import largeCloud from "./images/largeCloud.svg";
 import smallCloud from "./images/smallCloud.svg";
-import truck from "./images/truck.svg";
-import bigCar from "./images/bigCar.svg";
+import truck from "./images/car.svg";
+import Typewriter from "typewriter-effect";
 
 function HomePage() {
-  const handleType = () => {
-    console.log("hi");
-  };
-  const handleDone = () => {
-    console.log(`Done after 5 loops!`);
-  };
   return (
     <div id="landingPage" className={styles.landingPageContainer}>
       <section className={styles.aboutMeContainer}>
         <Image src={largeCloud} alt="cloud" id={styles.largeCloud} />
         <Image src={smallCloud} alt="cloud" id={styles.smallCloud} />
-        <div className={styles.myNameContainer}></div>
+        <div className={styles.myNameContainer}>
+          <h1 id={styles.myName}>Hi, I{"'"}m Mano!</h1>
+          <div id={styles.Iam}>
+            <span style={{ color: "black" }}>{"I'm"}</span>
+            <Typewriter
+              options={{
+                strings: [
+                  "<span style='color: rgb(190, 14, 120);'>Happy to meet you!</span>",
+                  "<span style='color: rgb(125, 29, 194);'>a Front-End Developer</span>",
+                  "<span style='color: rgb(132, 82, 11);'>passionate about Technology</span>",
+                  "<span style='color: rgb(160, 40, 100);'>a Computer Science Student</span>",
+                ],
+                skipAddStyles: true,
+                cursor: "",
+                delay: 50,
+                deleteSpeed: 35,
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+        </div>
         <div className={styles.balloonsContainer}>
           <Image
             src={balloonYellow}
@@ -37,7 +51,6 @@ function HomePage() {
       </section>
       <section className={styles.grass}>
         <Image src={truck} alt="car" id={styles.truck} />
-        {/* <Image src={bigCar} alt="bigCar" id={styles.bigCar} /> */}
         <div id={styles.triangle}></div>
       </section>
       <section className={styles.ground}></section>
