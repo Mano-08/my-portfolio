42f929157f80b4c3b895aebe5586d4b25624283a
Mano <mano.26q@gmail.com>
2023-02-04 19:15:18 +0530

    updated cloud animation

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -3,17 +3,25 @@ import Image from "next/image";
 import styles from "./HomePage.module.scss";
 import balloonYellow from "./images/yellowBalloon.svg";
 import balloonOrange from "./images/orangeBalloon.svg";
+import { Typewriter } from "react-simple-typewriter";
 import largeCloud from "./images/largeCloud.svg";
 import smallCloud from "./images/smallCloud.svg";
 import truck from "./images/truck.svg";
+import bigCar from "./images/bigCar.svg";
 
 function HomePage() {
+  const handleType = () => {
+    console.log("hi");
+  };
+  const handleDone = () => {
+    console.log(`Done after 5 loops!`);
+  };
   return (
     <div id="landingPage" className={styles.landingPageContainer}>
       <section className={styles.aboutMeContainer}>
         <Image src={largeCloud} alt="cloud" id={styles.largeCloud} />
         <Image src={smallCloud} alt="cloud" id={styles.smallCloud} />
-        <div className={styles.myNameContainer}>Hi Iam Mano</div>
+        <div className={styles.myNameContainer}></div>
         <div className={styles.balloonsContainer}>
           <Image
             src={balloonYellow}
@@ -29,6 +37,7 @@ function HomePage() {
       </section>
       <section className={styles.grass}>
         <Image src={truck} alt="car" id={styles.truck} />
+        {/* <Image src={bigCar} alt="bigCar" id={styles.bigCar} /> */}
         <div id={styles.triangle}></div>
       </section>
       <section className={styles.ground}></section>
